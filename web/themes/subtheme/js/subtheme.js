32dc9918363c6ab0c2bf949619460600e870231f
Ethan Saunders <ethansaunders117@Gmail.com>
2024-08-08 17:58:50 -0400

    First commit of site

---
+++ web/themes/subtheme/js/subtheme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Subtheme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.subtheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
